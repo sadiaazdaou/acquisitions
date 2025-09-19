69d09e546ab82bce085e2f0c5d4f461f268eedb3
sadiaazdaou <sadia.azdaou@gmail.com>
2025-09-19 10:06:23 -0400

    add server files

--- src/index.js
+++ src/index.js
@@ -1,10 +1,2 @@
-import express from 'express';
-
-const app = express();
-
-// Simple endpoints
-app.get('/', (req, res) => {
-  res.status(200).send('Salam from Acquisitions')
-});
-
-export default app;
+import 'dotenv/config';
+import './server.js';
--- src/server.js
+++ src/server.js
@@ -1,4 +1,4 @@
-import app from './index'
+import app from './app.js'
 
 const PORT = process.env.PORT ?? 3000;
 
